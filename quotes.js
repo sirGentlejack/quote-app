d1f18399de82a62745ee1f12fde5d3f85a648ef0
GentlejackB <fagbemiilemobayo@gmail.com>
2023-09-04 00:54:37 +0100

    first commit

---
+++ quotes.js
@@ -0,0 +1,36 @@
+var arrayOfQuotes = [
+  {
+    author: "Jim Rohn",
+    quotes: "Beware of what you become in pursuit of what you want.",
+  },
+  {
+    author: "Gentle Jack",
+    quotes: "YOLO - You Only Life Once.",
+  },
+  {
+    author: "Ilemobayo",
+    quotes: "Give your life to Christ.",
+  },
+  {
+    author: "King B-boy",
+    quotes: "I love wat i do.",
+  },
+  {
+    author: "Lemo B",
+    quotes: "simplicity is key.",
+  },
+];
+
+function randomSelector(arrayLength) {
+  return Math.floor(Math.random() * arrayLength);
+}
+
+function generateQuote() {
+  var randomNumber = randomSelector(arrayOfQuotes.length);
+
+  document.getElementById("quoteOutput").innerHTML =
+    '"' + arrayOfQuotes[randomNumber].quotes + '"';
+
+  document.getElementById("authorOutput").innerHTML =
+    "- " + arrayOfQuotes[randomNumber].author;
+}
